c77cb9e7da7123570bb455fe71fca1d420f37ee1
joabm <84096469+joabm@users.noreply.github.com>
2021-12-27 23:17:09 -0800

    added test file for performAction validation

---
+++ _test_/testApp.test.js
@@ -0,0 +1,7 @@
+import { performAction } from "../src/client/js/app"
+
+describe("Testing the submit functionality", () => {
+    test("Testing the handleSubmit() function", () => {
+
+           expect(performAction).toBeDefined();
+})});
