2bb397d26fc5abe2f4c9aec3a7814da2570b01c4
joabm <84096469+joabm@users.noreply.github.com>
2021-12-28 22:53:53 -0800

    updated test description

--- _test_/testApp.test.js
+++ _test_/testApp.test.js
@@ -1,7 +1,7 @@
 import "regenerator-runtime/runtime";
 import { performAction } from "../src/client/js/app";
 
-describe("Testing the performAction function truthiness", () => {
+describe("Testing the performAction function is defined", () => {
     test("Testing the performAction() function", () => {
 
            expect(performAction).toBeDefined();
